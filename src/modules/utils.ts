ac86e7f27b1e454ce5654d62f72b3dc075a31f8d
Dhia Rzig <dhiarzig@umich.edu>
2024-04-04 16:25:42 -0400

    Wrapped and tested LLM interaction Logic

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,21 +1,195 @@
 import OpenAI from 'openai';
+import fs from 'fs';
+import * as vscode from 'vscode';
+
+// load the config json
+import config from './config.json';
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
+
+// define interface for the config json file
+
+interface configJson {
+    LLM_Backend: string;
+    Endpoint?: string;
+    APIKey?: string;
+}
+
+export enum Backend {
+    'Azure',
+    'OpenAI',
+    'Copilot',
+}
+
+interface Config {
+    LLM_Backend: Backend;
+    Endpoint?: string;
+    APIKey?: string;
+}
+
+let configJson: configJson = config;
+
+let configuration: Config = {
+    LLM_Backend: Backend[config.LLM_Backend as keyof typeof Backend],
+};
+
+export function setBackend(backend: Backend) {
+    configuration.LLM_Backend = backend;
+    configJson.LLM_Backend = Backend[backend];
+    // save the new configuration to the config.json file
+    fs.writeFileSync('./config.json', JSON.stringify(configJson, null, 2));
+    return config;
+}
+
+export function getBackend(): Backend {
+    return configuration.LLM_Backend;
+}
+
+export function getEndpoint(): string | undefined {
+    return configuration.Endpoint;
+}
+export function getAPIKey(): string | undefined {
+    return configuration.APIKey;
+}
+
+export function setEndpoint(endpoint: string) {
+    configuration.Endpoint = endpoint;
+    configJson.Endpoint = endpoint;
+    fs.writeFileSync('./config.json', JSON.stringify(configJson, null, 2));
+    return config;
+}
+
+export function setAPIKey(APIKey: string) {
+    configuration.APIKey = APIKey;
+    configJson.APIKey = APIKey;
+    fs.writeFileSync('./config.json', JSON.stringify(configJson, null, 2));
+    return config;
+}
 
 export function getClient() {
-    if (process.env.AZURE_OPENAI_ENDPOINT === undefined) {
-        console.error('AZURE_OPENAI_ENDPOINT is undefined');
+    if (configuration.LLM_Backend === Backend.Azure) {
+        return getAzureClient();
+    } else if (configuration.LLM_Backend === Backend.OpenAI) {
+        return getOpenAIClient();
+    } else if (configuration.LLM_Backend === Backend.Copilot) {
+        return vscode.lm;
+    }
+}
+
+function getAzureClient() {
+    let endpoint: string | undefined;
+    let credential: string | undefined;
+    if (process.env.AZURE_OPENAI_ENDPOINT) {
+        endpoint = process.env.AZURE_OPENAI_ENDPOINT;
     } else {
-        const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
-        if (process.env.AZURE_OPENAI_API_KEY === undefined) {
-            console.error('AZURE_OPENAI_API_KEY is undefined');
+        if (getEndpoint()) {
+            endpoint = getEndpoint();
         } else {
-            const credential = process.env.AZURE_OPENAI_API_KEY;
-            let client = new OpenAI({
-                apiKey: credential,
-                baseURL: endpoint + 'openai/deployments/gpt-35-turbo',
-                defaultQuery: { 'api-version': '2023-05-15' },
-                defaultHeaders: { 'api-key': credential },
+            console.error('No endpoint provided in Environment or config.json');
+            return;
+        }
+    }
+    if (process.env.AZURE_OPENAI_API_KEY) {
+        credential = process.env.AZURE_OPENAI_API_KEY;
+    } else {
+        if (getAPIKey()) {
+            credential = getAPIKey();
+        } else {
+            console.error('No API Key provided in Environment or config.json');
+            return;
+        }
+    }
+    let client = new OpenAI({
+        apiKey: credential,
+        baseURL: endpoint + 'openai/deployments/gpt-35-turbo',
+        defaultQuery: { 'api-version': '2023-05-15' },
+        defaultHeaders: { 'api-key': credential },
+    });
+    return client;
+}
+
+function getOpenAIClient() {
+    throw new Error('Function not implemented.');
+}
+
+export async function sendChatRequest(
+    organizedMessages: ChatCompletionMessageParam[],
+    LLMOptions?: { [name: string]: any },
+    cancellationToken?: vscode.CancellationToken,
+    getDirectResponse?: boolean
+) {
+    let client = getClient();
+    if (client === undefined) {
+        console.error('Client is undefined');
+        return JSON.parse(
+            '{"error": " Issue during LLM Backend configuration"}'
+        );
+    } else {
+        // if backend is Azure or OpenAI
+        const otherOptions = LLMOptions?.filter(
+            (key: string) =>
+                key !== 'model' && key !== 'temperature' && key !== 'seed'
+        );
+        if (client instanceof OpenAI) {
+            const response = await client.chat.completions.create({
+                messages: organizedMessages,
+                model: (LLMOptions?.model as string) || 'gpt-35-turbo',
+                temperature: (LLMOptions?.temperature as number) || 0.3,
+                seed: (LLMOptions?.seed as number) || 42,
+                // transform remaining LLMOptions into parameter value pairs
+                ...otherOptions,
             });
-            return client;
+            if (getDirectResponse) {
+                return response;
+            } else {
+                const result = response.choices?.[0]?.message?.content;
+                if (result !== undefined && result !== null) {
+                    return result;
+                } else {
+                    console.error('No response from LLM');
+                    return '{"error": "No response from Azure OpenAI}"';
+                }
+            }
+        } else {
+            let convertedMessages: vscode.LanguageModelChatMessage[] = [];
+            organizedMessages.forEach((message) => {
+                if (message.role === 'system') {
+                    convertedMessages.push(
+                        new vscode.LanguageModelChatSystemMessage(
+                            message.content
+                        )
+                    );
+                } else if (message.role === 'user') {
+                    convertedMessages.push(
+                        new vscode.LanguageModelChatUserMessage(
+                            message.content as string
+                        )
+                    );
+                } else if (message.role === 'assistant') {
+                    convertedMessages.push(
+                        new vscode.LanguageModelChatAssistantMessage(
+                            message.content as string
+                        )
+                    );
+                } else {
+                    console.error('Invalid message role - skipping message');
+                }
+            });
+            const model =
+                (LLMOptions?.model as string) || 'copilot-gpt-3.5-turbo';
+            const result = await client.sendChatRequest(
+                model,
+                convertedMessages,
+                {
+                    modelOptions: LLMOptions,
+                },
+                cancellationToken || new vscode.CancellationTokenSource().token
+            );
+            let completeResult = '';
+            for await (const fragment of result.stream) {
+                completeResult += fragment;
+            }
+            return completeResult;
         }
     }
 }
+// TODO - Add more utility functions here
